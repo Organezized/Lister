2781b0fb5b5a32408e2970da82b9b2b035d60bc1
Damon Toumbourou <dtoumbourou@gmail.com>
2015-05-09 17:17:39 +1000

    Added list display and create functionality

---
+++ app/src/main/java/com/organezized/lister/DisplayListActivity.java
@@ -0,0 +1,70 @@
+package com.organezized.lister;
+
+import android.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+* Created by Damon on 9/05/15.
+*/
+
+public class DisplayListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_list);
+        Intent intent = getIntent();
+        String listName = intent.getStringExtra("LIST_NAME");
+        TextView textView = new TextView(this);
+        textView.setTextSize(50);
+        textView.setText(listName);
+
+        setContentView(textView);
+
+        /*
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+        */
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_list,
+                    container, false);
+            return rootView;
+        }
+    }
+}
--- app/src/main/java/com/organezized/lister/MainListActivity.java
+++ app/src/main/java/com/organezized/lister/MainListActivity.java
@@ -1,12 +1,17 @@
 package com.organezized.lister;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
 
 
 public class MainListActivity extends ActionBarActivity {
+    //public final static String EXTRA_MESSAGE = "Message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +41,16 @@ public class MainListActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void addItem(View view) {
+       Log.i("content","hello list");
+       Intent intent = new Intent(this, DisplayListActivity.class);
+       EditText editText = (EditText) findViewById(R.id.editText);
+       String listName = editText.getText().toString();
+       intent.putExtra("LIST_NAME", listName);
+       startActivity(intent);
+    }
+
+
+
 }
